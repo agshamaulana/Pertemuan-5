d9fcef717f1022193c21214b3a3a9872130b00ce
agshamaulana <agshamaulana14@students.amikom.ac.id>
2023-07-12 13:30:18 +0700

    first commit

---
+++ Number.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverload
+{
+    internal class Number
+    {
+        public void Min(int a, int b, int c)
+        {
+            Console.WriteLine("Tersedia angka sebagai berikut {0}, {1}, {2}", a, b, c);
+
+            if (a <= b && a <= c)
+            {
+
+                Console.WriteLine("Nilai terkecil dari ketiga angka adalah: {0}", a);
+            }
+            else if (b <= a && b <= c)
+            {
+
+                Console.WriteLine("Nilai terkecil dari ketiga angka adalah: {0}", b);
+            }
+            else if (c <= a && c <= b)
+            {
+
+                Console.WriteLine("Nilai terkecil dari ketiga angka adalah: {0}", c);
+
+            }
+        }
+
+
+        public int Max(int a, int b, int c)
+        {
+            Console.WriteLine("Tersedia angka sebagai berikut {0}, {1}, {2}", a, b, c);
+
+            if (a >= b && a >= c)
+            {
+
+                Console.WriteLine("Nilai terbesar dari ketiga angka adalah : {0}", a);
+            }
+            else if (b >= a && b >= c)
+            {
+
+                Console.WriteLine("Nilai terbesar dari ketiga angka adalah: {0}", b);
+            }
+            else if (c >= a && c <= b)
+            {
+
+                Console.WriteLine("Nilai terbesar dari ketiga angka adalah: {0}", c);
+            }
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Number method = new Number();
+            method.Min(27, 82, 11);
+
+            Console.WriteLine("--------------------------------------");
+            method.Max(50, 55, 30);
+
+            Console.ReadKey();
+        }
+    }
+}
